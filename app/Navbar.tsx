fc51099c6c60a0641ca03b3950ba3d0ec81dc260
Shravan Chaudhary <bitznatch1110@gmail.com>
2023-11-02 21:56:28 +0530

    setup eslint, prettier and tailwind config

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -3,19 +3,15 @@ import Link from 'next/link'
 const Navbar = () => {
   const links = [
     { label: 'Dashboard', href: '/' },
-    { label: 'Issues', href: '/issues' },
+    { label: 'Issues', href: '/issues' }
   ]
 
   return (
-    <nav className='flex space-x-6 border-b items-center h-14 mb-5 px-5'>
+    <nav className="mb-5 flex h-14 items-center space-x-6 border-b px-5">
       <Link href={'/'}>Logo</Link>
-      <ul className='flex space-x-6'>
+      <ul className="flex space-x-6">
         {links.map((link) => (
-          <Link
-            key={link.href}
-            href={link.href}
-            className='text-zinc-500 hover:text-zinc-800 transition-colors'
-          >
+          <Link key={link.href} href={link.href} className="text-zinc-500  transition-colors hover:text-zinc-800">
             {link.label}
           </Link>
         ))}
