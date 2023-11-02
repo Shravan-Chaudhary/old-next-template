c55e20ac4ea230f846b1b34ee7924e07dda0876c
Shravan Chaudhary <bitznatch1110@gmail.com>
2023-11-02 22:00:12 +0530

    remove navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-import Navbar from './Navbar'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,7 +15,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar />
         <main>{children}</main>
       </body>
     </html>
